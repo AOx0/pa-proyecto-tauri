4fe394d347737e0964e4c1de0a305d7502331c01
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-03-20 22:55:33 -0600

    Tauri init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,60 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use lazy_static::*;
+use std::process;
+use std::sync::{Arc, Mutex};
+
+use std::io::{BufRead, BufReader, Write};
+use subprocess::*;
+
+const ERR: u128 = 107492042;
+
+#[tauri::command]
+fn close() {
+  let mut p = PROC.lock().unwrap();
+  println!("Closing..");
+  if let Some(_) = p.poll() {
+    // the process has finished
+  } else {
+    // it is still running, terminate it
+    p.terminate().unwrap();
+  }
+  process::exit(0);
+}
+
+lazy_static! {
+  static ref PROC: Arc<Mutex<Popen>> = Arc::new(Mutex::new(
+    Popen::create(
+    &["python3", "/Users/alejandro/CLionProjects/fast/main.py"],
+    PopenConfig {
+      stdout: Redirection::Pipe,
+      stdin: Redirection::Pipe,
+      ..Default::default()
+    },
+  ).expect("couldn't spawn child command")));
+}
+
+#[tauri::command]
+fn display_upper_msg(msg: String) -> String {
+  let p = PROC.lock().unwrap();
+  let mut lines = BufReader::new(p.stdout.as_ref().unwrap()).lines();
+
+  let i: u128 = msg.trim().replace(" ", "").replace("\n", "").parse().unwrap_or(ERR);
+  if i == ERR { return format!("Error: Ingresa un número válido") }
+
+  p.stdin.as_ref().unwrap().write_all(format!("{i}\n").as_bytes()).unwrap();
+
+  format!("{}", lines.next().unwrap().unwrap())
+}
+
+fn main() {
+  lazy_static::initialize(&PROC);
+
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![close, display_upper_msg])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
